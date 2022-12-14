79e513f0edee259c60dfe01b679057fac3612666
Mohamed Hassan <mohamedhassan@Mohameds-Air.broadband>
2022-12-14 16:00:36 +0000

    initial commit

---
+++ theme.ts
@@ -0,0 +1,26 @@
+import { buildLegacyTheme } from "sanity";
+
+const props = {
+    '--my-white': '#fff',
+    '--my-black': '#1a1a1a',
+    '--momito': '#f7ab0a',
+    '--my-red': '#db4437',
+    '--my-yellow': '#f4b400',
+    '--my-green': '#0f9d58'
+};
+
+export const myTheme = buildLegacyTheme({
+    '--black': props['--my-black'],
+    '--white': props['--my-white'],
+
+    '--gray': '#666',
+    '--gray-base': '#666',
+
+    '--component-bg': props['--my-black'],
+    '--component-text-color': props['--my-white'],
+
+    '--brand-primary': props["--momito"],
+
+
+
+})
